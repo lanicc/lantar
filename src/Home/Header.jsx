c0fbcb37a2be307ff04c3dcba2849b953e2d36dc
lanicc <lanettiesso@gmail.com>
2024-07-16 08:39:05 +0800

    新文件：   .idea/.gitignore 	新文件：   .idea/encodings.xml 	新文件：   .idea/git_toolbox_blame.xml 	新文件：   .idea/inspectionProfiles/Project_Default.xml 	新文件：   .idea/lantar.iml 	新文件：   .idea/misc.xml 	新文件：   .idea/modules.xml 	新文件：   .idea/vcs.xml 	修改：     src/Home/Banner.jsx 	修改：     src/Home/Footer.jsx 	修改：     src/Home/Header.jsx 	修改：     src/Home/Page1.jsx 	修改：     src/Home/Page2.jsx

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Row, Col, Icon, Menu, Button, Popover } from 'antd';
+import {Icon, Menu, Popover} from 'antd';
 
-import { enquireScreen } from 'enquire-js';
-
-const LOGO_URL = 'https://gw.alipayobjects.com/zos/rmsportal/gVAKqIsuJCepKNbgbSwE.svg';
+import {enquireScreen} from 'enquire-js';
 
 class Header extends React.Component {
   state = {
@@ -13,34 +11,15 @@ class Header extends React.Component {
 
   componentDidMount() {
     enquireScreen((b) => {
-      this.setState({ menuMode: b ? 'inline' : 'horizontal' });
+      this.setState({menuMode: b ? 'inline' : 'horizontal'});
     });
   }
 
   render() {
-    const { menuMode, menuVisible } = this.state;
+    const {menuMode, menuVisible} = this.state;
 
     const menu = (
-      <Menu mode={menuMode} id="nav" key="nav">
-        <Menu.Item key="home">
-          <a>首页</a>
-        </Menu.Item>
-        <Menu.Item key="docs">
-          <a><span>文档</span></a>
-        </Menu.Item>
-        <Menu.Item key="components">
-          <a>组件</a>
-        </Menu.Item>
-        {
-          menuMode === 'inline' && (
-            <Menu.Item key="preview">
-              <a target="_blank" href="http://preview.pro.ant.design/" rel="noopener noreferrer">
-                预览
-              </a>
-            </Menu.Item>
-          )
-        }
-      </Menu>
+      <Menu mode={menuMode} id="nav" key="nav" />
     );
 
     return (
@@ -62,31 +41,6 @@ class Header extends React.Component {
             />
           </Popover>
         ) : null}
-        <Row>
-          <Col xxl={4} xl={5} lg={8} md={8} sm={24} xs={24}>
-            <div id="logo" to="/">
-              <img src={LOGO_URL} alt="logo" />
-              <span>ANT DESIGN PRO</span>
-            </div>
-          </Col>
-          <Col xxl={20} xl={19} lg={16} md={16} sm={0} xs={0}>
-            <div className="header-meta">
-              <div id="preview">
-                <a
-                  id="preview-button"
-                  target="_blank"
-                  href="http://preview.pro.ant.design"
-                  rel="noopener noreferrer"
-                >
-                  <Button icon="eye-o">
-                    预览
-                  </Button>
-                </a>
-              </div>
-              {menuMode === 'horizontal' ? <div id="menu">{menu}</div> : null}
-            </div>
-          </Col>
-        </Row>
       </div>
     );
   }
--- src/Home/Page1.jsx
+++ src/Home/Page1.jsx
@@ -1,97 +1,91 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
-import Parallax from 'rc-scroll-anim/lib/ScrollParallax';
 import QueueAnim from 'rc-queue-anim';
 import TweenOne from 'rc-tween-one';
 
-const { TweenOneGroup } = TweenOne;
+const {TweenOneGroup} = TweenOne;
 
 const featuresCN = [
   {
-    title: '优雅美观',
-    content: '基于 Ant Design 体系精心设计',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/VriUmzNjDnjoFoFFZvuh.svg',
+    title: '网站建设',
+    content: '企业建站 定制网站 高端大气',
+    src: 'https://gw.alipayobjects.com/zos/rmsportal/BISfzKcCNCYFmTYcUygW.svg',
     color: '#13C2C2',
     shadowColor: 'rgba(19,194,194,.12)',
   },
   {
-    title: '常见设计模式',
-    content: '提炼自中后台应用的典型页面和场景',
+    title: 'OA办公',
+    content: '提高效率 可对接钉钉、企微等',
     src: 'https://gw.alipayobjects.com/zos/rmsportal/smwQOoxCjXVbNAKMqvWk.svg',
     color: '#2F54EB',
     shadowColor: 'rgba(47,84,235,.12)',
   },
   {
-    title: '最新技术栈',
-    content: '使用 React/dva/antd 等前端前沿技术开发',
+    title: '小程序、APP',
+    content: '多端小程序开发、网站开发、微信公众号开发、H5开发、App开发',
     src: 'https://gw.alipayobjects.com/zos/rmsportal/hBbIHzUsSbSxrhoRFYzi.svg',
     color: '#F5222D',
     shadowColor: 'rgba(245,34,45,.12)',
   },
   {
-    title: '响应式',
-    content: '针对不同屏幕大小设计',
+    title: 'MES生产信息化',
+    content: '制造数据管理、排查调度、生产过程监控',
     src: 'https://gw.alipayobjects.com/zos/rmsportal/BISfzKcCNCYFmTYcUygW.svg',
     color: '#1AC44D',
     shadowColor: 'rgba(26,196,77,.12)',
   },
   {
-    title: '主题',
-    content: '可配置的主题满足多样化的品牌诉求',
+    title: 'CRM客户管理',
+    content: '客户资料管理、合同管理，信息数字化',
     src: 'https://gw.alipayobjects.com/zos/rmsportal/XxqEexmShHOofjMYOCHi.svg',
     color: '#FAAD14',
     shadowColor: 'rgba(250,173,20,.12)',
   },
   {
-    title: '国际化',
-    content: '内建业界通用的国际化方案（敬请期待）',
+    title: 'MF-MTS生产追溯系统',
+    content: '工艺作业自动记录，精益生产和管理，防错、监控、实时分析和追踪',
     src: 'https://gw.alipayobjects.com/zos/rmsportal/JsixxWSViARJnQbAAPkI.svg',
     color: '#722ED1',
     shadowColor: 'rgba(114,46,209,.12)',
   },
   {
-    title: '最佳实践',
-    content: '良好的工程实践助你持续产出高质量代码',
+    title: '建设企业数仓系统',
+    content: '整合数据，通过数据支撑决策制定过程',
     src: 'https://gw.alipayobjects.com/zos/rmsportal/pbmKMSFpLurLALLNliUQ.svg',
     color: '#FA8C16',
     shadowColor: 'rgba(250,140,22,.12)',
   },
   {
-    title: 'Mock 数据',
-    content: '实用的本地数据调试方案',
+    title: '建设企业数据分析平台',
+    content: '通过图表让企业更好的理解过去、分析现状、预测未来',
     src: 'https://gw.alipayobjects.com/zos/rmsportal/aLQyKyUyssIUhHTZqCIb.svg',
     color: '#EB2F96',
     shadowColor: 'rgba(235,45,150,.12)',
-  },
-  {
-    title: 'UI 测试',
-    content: '自动化测试保障前端产品质量',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/RpJIQitGbSCHwLMimybX.svg',
-    color: '#1890FF',
-    shadowColor: 'rgba(24,144,255,.12)',
-  },
+  }
 ];
 
 const pointPos = [
-  { x: -30, y: -10 },
-  { x: 20, y: -20 },
-  { x: -65, y: 15 },
-  { x: -45, y: 80 },
-  { x: 35, y: 5 },
-  { x: 50, y: 50, opacity: 0.2 },
+  {x: -30, y: -10},
+  {x: 20, y: -20},
+  {x: -65, y: 15},
+  {x: -45, y: 80},
+  {x: 35, y: 5},
+  {x: 50, y: 50, opacity: 0.2},
 ];
 
 class Page1 extends React.PureComponent {
   static propTypes = {
     isMobile: PropTypes.bool.isRequired,
   }
+
   constructor(props) {
     super(props);
     this.state = {
       hoverNum: null,
     };
   }
+
   onMouseOver = (i) => {
     this.setState({
       hoverNum: i,
@@ -118,8 +112,9 @@ class Page1 extends React.PureComponent {
         repeat: -1,
       }];
   }
+
   render() {
-    const { hoverNum } = this.state;
+    const {hoverNum} = this.state;
     let children = [[], [], []];
     featuresCN.forEach((item, i) => {
       const isHover = hoverNum === i;
@@ -138,10 +133,12 @@ class Page1 extends React.PureComponent {
         />
       ));
       const child = (
-        <li key={i.toString()} >
+        <li key={i.toString()}>
           <div
             className="page1-box"
-            onMouseEnter={() => { this.onMouseOver(i); }}
+            onMouseEnter={() => {
+              this.onMouseOver(i);
+            }}
             onMouseLeave={this.onMouseOut}
           >
             <TweenOneGroup
@@ -161,14 +158,14 @@ class Page1 extends React.PureComponent {
                   '0 6px 12px'} ${item.shadowColor}`,
               }}
             >
-              <img src={item.src} alt="img" style={i === 4 ? { marginLeft: -15 } : {}} />
+              <img src={item.src} alt="img" style={i === 4 ? {marginLeft: -15} : {}} />
             </div>
             <h3>{item.title}</h3>
             <p>{item.content}</p>
           </div>
         </li>
       );
-      children[Math.floor(i / 3)].push(child);
+      children[Math.floor(i / 4)].push(child);
     });
 
     children = children.map((item, i) => (
@@ -184,18 +181,9 @@ class Page1 extends React.PureComponent {
       </QueueAnim>
     ));
     return (
-      <div className="home-page page1" >
+      <div className="home-page page1">
         <div className="home-page-wrapper" id="page1-wrapper">
-          {!this.props.isMobile && (
-            <Parallax
-              className="page1-bg"
-              animation={{ translateY: 200, ease: 'linear', playScale: [0, 1.65] }}
-              location="page1-wrapper"
-            >
-              Feature
-            </Parallax>
-          )}
-          <h2>What can <span>Pro</span> do for you </h2>
+          <h2>我们可以 <span>为您</span> 做什么 </h2>
           <div className="title-line-wrapper page1-line">
             <div className="title-line" />
           </div>
